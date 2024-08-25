fd498dcac7b3fc5d18beae9a0949a0f1bbca2259
lucas pedroso <pedrosolucas1745@gmail.com>
2024-08-25 02:16:57 -0300

    refactor: refactor listenEmailqueue to print data of messages

--- src/main/java/com/ms/email/Consumers/EmailConsumer.java
+++ src/main/java/com/ms/email/Consumers/EmailConsumer.java
@@ -1,5 +1,6 @@
 package com.ms.email.Consumers;
 
+import com.ms.email.DTO.EmailDTO;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
@@ -8,7 +9,9 @@ import org.springframework.stereotype.Component;
 public class EmailConsumer
 {
     @RabbitListener(queues = "${broker.queue.email.name}")
-    public void listenEmailQueue(@Payload String payload){
-        System.out.println(payload);
+    public void listenEmailQueue(@Payload EmailDTO dto){
+        System.out.println(dto.mailTo());
+        System.out.println(dto.subject());
+        System.out.println(dto.text());
     }
 }
