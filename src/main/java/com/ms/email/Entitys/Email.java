9228b6938c83eafbe79ab74de7589818dada12b3
lucas pedroso <pedrosolucas1745@gmail.com>
2025-01-15 17:03:36 -0300

    add: add entity for DB to save emails

---
+++ src/main/java/com/ms/email/Entitys/Email.java
@@ -0,0 +1,60 @@
+package com.ms.email.Entitys;
+
+import jakarta.persistence.*;
+
+import java.util.UUID;
+
+@Entity
+@Table(name = "emails")
+public class Email {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private UUID id;
+    private UUID userId;
+    private String subject;
+    private String text;
+    private String mailTo;
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getSubject() {
+        return subject;
+    }
+
+    public void setSubject(String subject) {
+        this.subject = subject;
+    }
+
+    public String getText() {
+        return text;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public String getMailTo() {
+        return mailTo;
+    }
+
+    public void setMailTo(String mailTo) {
+        this.mailTo = mailTo;
+    }
+
+    public UUID getUserId() {
+        return userId;
+    }
+
+    public void setUserId(UUID userId) {
+        this.userId = userId;
+    }
+
+    public Email(UUID userId, String subject, String text, String mailTo) {
+        this.userId = userId;
+        this.subject = subject;
+        this.text = text;
+        this.mailTo = mailTo;
+    }
+}
