9cd5f65e5f234ea8305fa44bb452242ec8cfb0ce
lucas pedroso <pedrosolucas1745@gmail.com>
2025-01-15 17:58:46 -0300

    refactor: add adress emailFrom, and datetime in object to save

--- src/main/java/com/ms/email/Services/EmailService.java
+++ src/main/java/com/ms/email/Services/EmailService.java
@@ -4,22 +4,26 @@ import com.ms.email.DTO.EmailDTO;
 import com.ms.email.Entitys.Email;
 import com.ms.email.Repositorys.EmailRepository;
 import org.springframework.stereotype.Service;
+import java.time.LocalDateTime;
 
 @Service
 public class EmailService {
 
     private final EmailRepository emailRepository;
-
+    private static final String mail = "pedrosolucas1745@gmail.com";
     public EmailService(EmailRepository emailRepository) {
         this.emailRepository = emailRepository;
     }
 
     public Email create(EmailDTO dto){
+        LocalDateTime now = LocalDateTime.now();
         Email email = new Email(
                 dto.userId(),
                 dto.subject(),
                 dto.text(),
-                dto.mailTo()
+                dto.mailTo(),
+                mail,
+                now
         );
         return emailRepository.save(email);
     }
